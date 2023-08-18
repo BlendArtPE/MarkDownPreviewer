577b3a25135d2fad582b357a1ac03e7bac0e443d
Daniel <jesus.cs.7.2010@gmail.com>
2023-08-18 00:55:22 -0500

    CSS finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,34 @@ function App() {
 
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="well"></div>
-            <textarea name="" id="editor" cols="30" rows="10"></textarea>
-          </div>
-          <div className="col-md-6">
-            <div id="preview">
-              
+      <div className="container-fluid text-center text-bg-secondary ">
+        <div className="row vh-100">
+          <div className="col-md-5 d-flex justify-content-center align-items-center">
+            <div className="card">
+              <div className="card-body">
+                  <div className="card-title">
+                    <h2>Editor</h2>
+                  </div>
+                  <div className="card-text">
+                    <textarea className='form-control' name="" id="editor" cols="50" rows="15"></textarea>
+                  </div>
+              </div>
             </div>
           </div>
+          <div className="col-md-2 text-bg-info">
+            
+          </div>
+          <div className="col-md-5 d-flex justify-content-center align-items-center">
+            <div className="card">
+              <div className="card-body">
+                  <div className="card-title">
+                    <h2>Previewer</h2>
+                  </div>
+                  <div className="card-text" id='preview'>
+                  </div>
+              </div>
+            </div>
+        </div>
         </div>
       </div>
     </>
